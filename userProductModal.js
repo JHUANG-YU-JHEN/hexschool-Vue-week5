e211fe7d71c29f84f14e0fc4fbc20450a0388da4
JHUANG-YU-JHEN <df89025338950@gmail.com>
2024-03-24 23:58:44 +0800

    git init

---
+++ userProductModal.js
@@ -0,0 +1,33 @@
+export default {
+  template: '#userProductModal',
+  props: {
+    product: {
+      type: Object,
+      default() {
+        return {
+        }
+      }
+    }
+  },
+  data() {
+    return {
+      status: {},
+      modal: '',
+      qty: 1,
+    };
+  },
+  mounted() { //new XXXX 函式建構子 ，只要看到new後面的函式建構子命名就會是大寫
+    this.modal = new bootstrap.Modal(this.$refs.modal, {
+      keyboard: false,
+      backdrop: 'static'
+    });
+  },
+  methods: {
+    openModal() {
+      this.modal.show();
+    },
+    hideModal() {
+      this.modal.hide();
+    },
+  },
+}
